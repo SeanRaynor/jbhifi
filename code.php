cde9b88e529e75b75ced40c99e7c666360447cf2
SeanRaynor <39557997+SeanRaynor@users.noreply.github.com>
2024-02-26 12:16:17 +0800

    Update code.php

--- code.php
+++ code.php
@@ -7,6 +7,7 @@ class JBHIFI{
         //set timezone to AU/Melbourne
         date_default_timezone_set('Australia/Melbourne');
 
+    
         //Set configurables
         $affilliateid = '15914';
         $ts = date('Ymd_His', time()); //20220824_100426
@@ -15,27 +16,17 @@ class JBHIFI{
         //generate MD5 hash
         $hash = md5(''.$affilliateid.''.$ts.''.$secret.'');
 
+       
+
         //build URL
         $generatedURL = $baseUrl . '?affilliateid=' . $affilliateid . '&ts=' . $ts . '&hash=' . $hash;
         return $generatedURL;
     }
 
     public static function redirectToGeneratedURL(){
-        // Specify the allowed IP address
-        $allowedIP = '123.103.216.74'; // Change this to the IP you want to allow
-
-        // Get the IP address of the client
-        $clientIP = $_SERVER['REMOTE_ADDR'];
-
-        // Check if the client IP matches the allowed IP
-        if ($clientIP === $allowedIP) {
-            $url = self::generateJBCorporateBenefitsURL();
-            header('Location: ' . $url);
-            exit; // Ensure script execution ends here if redirected
-        } else {
-            echo "Access denied."; // Message displayed if the IP doesn't match
-            echo $clientIP;
-        }
+        $url = self::generateJBCorporateBenefitsURL();
+        header('Location: ' . $url);
+        exit; // Ensure script execution ends here
     }
 }
 
